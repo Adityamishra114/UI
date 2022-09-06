4b3d899525fb30b6364ec224eff153dec85a5b71
Adityamishra114 <amishradbg1997@gmail.com>
2022-09-06 16:51:07 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -109,6 +109,39 @@ const MainContainer = styled.div`
   border-radius: 10px;
   color: #ffffff;
   letter-spacing: 0.4rem;
+  @media only screen and (max-width: 320px) {
+    width: 80vw;
+    height: 90vh;
+    hr {
+      margin-bottom: 0.3rem;
+    }
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 360px) {
+    width: 80vw;
+    height: 90vh;
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 411px) {
+    width: 80vw;
+    height: 90vh;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 80vw;
+    height: 80vh;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 70vw;
+    height: 50vh;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 30vw;
+    height: 80vh;
+  }
 `;
 const InputContainer = styled.div`
   display: flex;
